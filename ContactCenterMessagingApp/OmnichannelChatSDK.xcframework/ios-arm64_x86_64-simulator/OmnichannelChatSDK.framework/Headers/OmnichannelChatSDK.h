b15af1bd3d02d78476af1a4d33d322d362fb98b3
Mugdha Mundhe <89035654+MugdhaSMundhe@users.noreply.github.com>
2025-01-18 03:03:10 +0530

    OmnichannelChatSDK.xcframework added

---
+++ ContactCenterMessagingApp/OmnichannelChatSDK.xcframework/ios-arm64_x86_64-simulator/OmnichannelChatSDK.framework/Headers/OmnichannelChatSDK.h
@@ -0,0 +1,16 @@
+//
+//  OmnichannelChatSDK.h
+//  OmnichannelChatSDK
+//
+//  Created by Microsoft on 14/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OmnichannelChatSDK.
+FOUNDATION_EXPORT double OmnichannelChatSDKVersionNumber;
+
+//! Project version string for OmnichannelChatSDK.
+FOUNDATION_EXPORT const unsigned char OmnichannelChatSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OmnichannelChatSDK/PublicHeader.h>
