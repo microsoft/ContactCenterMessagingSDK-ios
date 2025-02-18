9124cfdda93b3b85ebfcdbb48535cd21fb7c5f0f
Mugdha Mundhe <89035654+MugdhaSMundhe@users.noreply.github.com>
2025-02-18 17:31:08 +0530

    Adaptivecards dependency removed
    remote binary added

--- Package.swift
+++ Package.swift
@@ -7,22 +7,17 @@ let package = Package(
     name: "ContactCenterMessagingSDK-ios",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v14),
-        .macOS(.v12)
+        .iOS(.v14)
     ],
     products: [
         .library(
             name: "ContactCenterMessagingSDK-ios",
             targets: ["ContactCenterMessagingSDK-ios"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/microsoft/AdaptiveCards.git", from: "2022.12.0")
-    ],
     targets: [
         .target(
             name: "ContactCenterMessagingSDK-ios",
-            dependencies: [.target(name: "ContactCenterMessagingSDK"),
-                           .product(name: "AdaptiveCards", package: "AdaptiveCards")]),
+            dependencies: [.target(name: "ContactCenterMessagingSDK")]),
         .binaryTarget(
             name: "ContactCenterMessagingSDK",
             url: "https://github.com/microsoft/ContactCenterMessagingSDK-ios/releases/download/v0.0.2/ContactCenterMessagingSDK.zip",
